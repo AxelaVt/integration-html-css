196709feb8ec497847caf73ea7090b72c75000f7
Alexa Vermenot <alexa.vermenot@orange.fr>
2020-05-06 16:48:51 +0200

    transformation du html en php

--- index.php
+++ index.php
@@ -1,46 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Tuncay Basak">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Titre</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="main.css">
-</head>
+<?php
 
-<body class="main">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="https://www.accesscodeschool.fr/"><img id="logo-acs" src="img/logo_acs_noir.png" alt="Logo acces code school"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="html.html">html</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="css.html">css</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="console.html">command line</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="git.html">git & github</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="atom.html">Atom</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
+  include "header.php"; ?>
 
 
   <main class="container">
@@ -134,9 +95,4 @@
       </div>
     </div>
   </main>
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-
-</html>
+  <?php include "footer.php";
